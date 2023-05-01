137bd6e9fdb300d204eb1d7153b2522dcedb99eb
Nico <menciere@amazon.com>
2023-05-01 16:18:34 -0400

    Update Footer and yarn.lock

--- dapp/src/components/modules/Footer/Footer.js
+++ dapp/src/components/modules/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <nav className="bg-[#181e27] pb-10 pt-10 h-max">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-max">
-        <a className="text-white" href="" target="_blank">
+        <a className="text-white" href="https://github.com/aws-samples/aws-nft-gallery-web3-cognito" target="_blank">
             Github project repository
         </a>
       </div>
